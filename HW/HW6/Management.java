bf768a87bb306a5cb3a9a561c0e8aa2cd7cda287
yakhina <1we@>
2023-10-24 16:37:54 +0300

    ДЗ 6. ООП Дизайн и Solid

---
+++ HW/HW6/Management.java
@@ -0,0 +1,24 @@
+package HW.HW6;
+
+public class Management extends Teacher {
+    int category; // категория руководителей
+
+    // 1 - руководители низового звена (down management)
+    // 2 - операционные управляющие руководители среднего звена (middle management)
+    // 3 - руководители высшего звена (top management)
+    
+    public Management(String lastName, String firstName, String patronymic, int category) {
+        super(lastName, firstName, patronymic);
+        this.category = category;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    @Override
+    public String toString() {
+        return "ФИО: " + lastName + ' ' + firstName + ' ' + patronymic + ' ' +
+                "категория: " + category;
+    }
+}
