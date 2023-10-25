92132136273d65f619330d15258150b1e5a79be3
yakhina <1we@>
2023-10-25 09:26:43 +0300

    Созданы абстрактные классы UserBase & UserBaseGroup.

--- HW/HW6/User/User.java
+++ HW/HW6/User/User.java
@@ -3,31 +3,49 @@ package HW.HW6.User;
 import HW.HW6.AbstractClass.UserBase;
 
 public class User extends UserBase {
-    String firstName;
-    String lastName;
 
-    public User(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    protected String firstName; // модификатором доступа protected: поля видны в пределах всех классов, находящихся в том же пакете,в пределах всех классов-наследников нашего класса.
+    protected String lastName;
+    protected String patronymic;
+
+    // Принцип полиморфизма, то есть в зависимости от переданного типа данных можно применять одну и ту же функцию
+
+    public User(String firstName, String lastName, String patronymic) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setPatronymic(patronymic);
     }
-
     public User() {
     }
 
-    ;
-
     public String getFirstName() {
         return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
     public String getLastName() {
         return lastName;
     }
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
     @Override
     public String toString() {
-        return "Пользователь:' " +
-                "Фамилия = '" + firstName + '\'' +
-                ", Имя = '" + lastName + '\'';
+        return "Пользователь: " +
+                "Имя: " + firstName + '\'' +
+                ", Фамилия: " + lastName + '\'' +
+                ", Отчество: " + patronymic + '\'';
     }
 }
---
+++ HW/HW6/User/UserGroup.java
@@ -0,0 +1,25 @@
+package HW.HW6.User;
+
+import HW.HW6.AbstractClass.UserBaseGroup;
+
+public class UserGroup extends UserBaseGroup {
+    int groupNumber;
+
+    public UserGroup(){
+
+    }
+
+    public int getGroupNumber() {
+        return groupNumber;
+    }
+
+    public void setGroupNumber(int groupNumber) {
+        this.groupNumber = groupNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "ГРУППА: " +
+                "номер группы: " + groupNumber;
+    }
+}
