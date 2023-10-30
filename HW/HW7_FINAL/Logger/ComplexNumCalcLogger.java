caf26df2e6b7d9ff44d809f5461a872750f63d0f
yakhina <1we@>
2023-10-30 10:55:12 +0300

    Логгер с выводом сообщений ComplexNumCalcLogger

--- HW/HW7_FINAL/Logger/ComplexNumCalcLogger.java
+++ HW/HW7_FINAL/Logger/ComplexNumCalcLogger.java
@@ -4,7 +4,6 @@ import HW.HW7_FINAL.Calc.ICalculator;
 import HW.HW7_FINAL.Model.ComplexNumber;
 
 public class ComplexNumCalcLogger implements ICalculator<ComplexNumber> {
-
     private ICalculator calcLog;
     private ILogger log;
 
@@ -12,32 +11,31 @@ public class ComplexNumCalcLogger implements ICalculator<ComplexNumber> {
         this.calcLog = calcLog;
         this.log = log;
     }
-
     @Override
     public ComplexNumber sum(ComplexNumber t1, ComplexNumber t2) {
         ComplexNumber result = (ComplexNumber) calcLog.sum(t1, t2);
-        log.loggerMessage(String.format("Вызван метод sum: %s + %s = %s", t1, t2, result));
+        log.loggerMessage(String.format("Вызов метода sum(сложение): %s + %s = %s", t1, t2, result));
         return result;
     }
 
     @Override
     public ComplexNumber difference(ComplexNumber t1, ComplexNumber t2) {
         ComplexNumber result = (ComplexNumber) calcLog.difference(t1, t2);
-        log.loggerMessage(String.format("Вызван метод difference: %s - %s = %s", t1, t2, result));
+        log.loggerMessage(String.format("Вызов метода difference(вычитание): %s - %s = %s", t1, t2, result));
         return result;
     }
 
     @Override
     public ComplexNumber multiplication(ComplexNumber t1, ComplexNumber t2) {
         ComplexNumber result = (ComplexNumber) calcLog.multiplication(t1, t2);
-        log.loggerMessage(String.format("Вызван метод multiplication: %s * %s = %s", t1, t2, result));
+        log.loggerMessage(String.format("Вызов метода multiplication(умножение): %s * %s = %s", t1, t2, result));
         return result;
     }
 
     @Override
     public ComplexNumber divide(ComplexNumber t1, ComplexNumber t2) {
         ComplexNumber result = (ComplexNumber) calcLog.divide(t1, t2);
-        log.loggerMessage(String.format("Вызван метод divide: %s / %s = %s", t1, t2, result));
+        log.loggerMessage(String.format("Вызов метода divide(деление): %s / %s = %s", t1, t2, result));
         return result;
     }
 }
